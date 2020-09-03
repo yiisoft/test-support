68dd3b416b0fc1327e0faf8ff3613170c43ffd93
roxblnfk <roxblnfk@ya.ru>
2020-09-03 22:11:19 +0300

    Merge SimpleContainer with ClosureContainer

--- tests/Container/SimpleContainerTest.php
+++ tests/Container/SimpleContainerTest.php
@@ -4,12 +4,34 @@ declare(strict_types=1);
 
 namespace Yiisoft\Test\Support\Tests\Container;
 
+use Closure;
 use Yiisoft\Test\Support\Container\SimpleContainer;
 
 final class SimpleContainerTest extends BaseContainerTest
 {
-    protected function createContainer(array $definitions = []): SimpleContainer
+    public function testGetFromCustomClosure(): void
     {
-        return new SimpleContainer($definitions);
+        $container = $this->createContainer([], static fn ($id) => $id);
+
+        $this->assertSame('foo', $container->get('foo'));
+    }
+
+    public function testHasFromCustomClosure(): void
+    {
+        $container = $this->createContainer([], static fn ($id) => $id);
+
+        $this->assertTrue($container->has('foo'));
+    }
+
+    public function testLowClosurePriority(): void
+    {
+        $container = $this->createContainer(['foo' => 'foo'], static fn ($id) => 'bar');
+
+        $this->assertSame('foo', $container->get('foo'));
+    }
+
+    protected function createContainer(array $definitions = [], Closure $closure = null): SimpleContainer
+    {
+        return new SimpleContainer($definitions, $closure);
     }
 }
