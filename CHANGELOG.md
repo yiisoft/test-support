e23e7b53084f58630847b73b58a4f373c1b8ea7b
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2022-03-20 19:02:36 +0300

    Add custom callback and fix default behavior for `SimpleContainer::has()` (#40)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,9 @@
 # Yii Test Support Change Log
 
+## 1.4.0 under development
 
-## 1.3.1 under development
-
-- no changes in this release.
-
+- Fix #40: Catch only `NotFoundException` instead of `Throwable` in `SimpleContainer::has()` method (vjik)
+- Enh #40: Add custom callback for method `has()` of `SimpleContainer` (vjik)
 
 ## 1.3.0 March 15, 2021
 
@@ -25,4 +24,3 @@
 ## 1.0.0 December 24, 2020
 
 - Initial release.
-
--- src/Container/SimpleContainer.php
+++ src/Container/SimpleContainer.php
@@ -6,25 +6,54 @@ namespace Yiisoft\Test\Support\Container;
 
 use Closure;
 use Psr\Container\ContainerInterface;
-use Throwable;
 use Yiisoft\Test\Support\Container\Exception\NotFoundException;
+
 use function array_key_exists;
 
 final class SimpleContainer implements ContainerInterface
 {
     private array $definitions;
+
+    /**
+     * @psalm-var Closure(string)
+     */
     private Closure $factory;
+
+    /**
+     * @psalm-var Closure(string): bool
+     */
+    private Closure $hasCallback;
 
     /**
      * @param array $definitions
      * @param Closure|null $factory Should be closure that works like ContainerInterface::get(string $id): mixed
+     * @param Closure|null $hasCallback Should be closure that works like ContainerInterface::has(string $id): bool
+     *
+     * @psalm-param Closure(string) $factory
+     * @psalm-param Closure(string):bool $hasCallback
      */
-    public function __construct(array $definitions = [], Closure $factory = null)
-    {
+    public function __construct(
+        array $definitions = [],
+        ?Closure $factory = null,
+        ?Closure $hasCallback = null
+    ) {
         $this->definitions = $definitions;
-        $this->factory = $factory ?? static function (string $id): void {
-            throw new NotFoundException($id);
-        };
+
+        $this->factory = $factory ??
+            /** @return mixed */
+            static function (string $id) {
+                throw new NotFoundException($id);
+            };
+
+        $this->hasCallback = $hasCallback ??
+            function (string $id): bool {
+                try {
+                    $this->get($id);
+                    return true;
+                } catch (NotFoundException $e) {
+                    return false;
+                }
+            };
     }
 
     public function get($id)
@@ -40,11 +69,6 @@ final class SimpleContainer implements ContainerInterface
         if (array_key_exists($id, $this->definitions)) {
             return true;
         }
-        try {
-            $this->get($id);
-            return true;
-        } catch (Throwable $e) {
-            return false;
-        }
+        return ($this->hasCallback)($id);
     }
 }
